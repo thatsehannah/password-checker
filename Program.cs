a7e025e18c866858434e5fd06dc88a4059c5a069
E. Hannah <echannah631@gmail.com>
2023-08-09 11:00:19 -0500

    password validator function

--- Program.cs
+++ Program.cs
@@ -1,3 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace PasswordChecker
+{
+    class Program
+    {
+        static int passwordValidator(string password)
+        {
+            int score = 0;
+
+            int minLength = 8;
+            string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string lowercase = "abcdefghijklmnopqrstuvwxyz";
+            string digits = "0123456789";
+            string specialChars = "$%&*!@#_";
+
+            if (password.Length >= minLength)
+            {
+                score++;
+            }
+
+            if (password.IndexOfAny(uppercase.ToCharArray()) != -1)
+            {
+                score++;
+            }
+
+            if (password.IndexOfAny(lowercase.ToCharArray()) != -1)
+            {
+                score++;
+            }
+
+            if (password.IndexOfAny(digits.ToCharArray()) != -1)
+            {
+                score++;
+            }
+
+            if (password.IndexOfAny(specialChars.ToCharArray()) != -1)
+            {
+                score++;
+            }
+
+            return score;
+        }
+
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
 
